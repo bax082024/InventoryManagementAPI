ae0d20b0601b6b52918e9dd2fd4249bed0bdacc2
bax082024 <bax082024@gmail.com>
2024-10-21 23:22:02 +0200

    started inventory list

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using InventoryManagementAPI.Models;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -6,7 +6,18 @@ namespace InventoryManagementAPI.Services
 {
   public class InventoryService : IInventoryService
   {
-    
+    private readonly List<Item> _items;
+
+    public InventoryService()
+    {
+      _items = new List<Item>
+      {
+        new Item {Id = 1, Name = "random1", Quantity = 5, Price = 45M, Description = "Random1"},
+        new Item {Id = 2, Name = "random2", Quantity = 25, Price = 147M, Description = "Random2"}
+      };
+    }
+
+
   }
 
 
