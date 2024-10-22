34314a96d8629c0776a0209765cd6e05cf2245a6
bax082024 <bax082024@gmail.com>
2024-10-22 19:55:53 +0200

    fixed synax error

--- Services/InventoryService.cs
+++ Services/InventoryService.cs
@@ -14,7 +14,7 @@ namespace InventoryManagementAPI.Services
       {
 
         new Item {Id = 1, Name = "Post it 10pk", Quantity = 10, Price = 75M, Description = "Post it pads 10 pack"},
-        new Item {Id = 2, Name = "Pen Uniball 12pcs", Quantity = 12, Price = 165M, Description = "uniball 207 Needle Retractable Gel Pens, Medium Point, 0.7mm, Blue Ink, Dozen"}
+        new Item {Id = 2, Name = "Pen Uniball 12pcs", Quantity = 12, Price = 165M, Description = "uniball 207 Needle Retractable Gel Pens, Medium Point, 0.7mm, Blue Ink, Dozen"},
         new Item {Id = 1, Name = "Sharpie Retractable S-Gel", Quantity = 12, Price = 180M, Description = "Sharpie S-Gel Retractable Gel Pen, Medium Point, Black Ink, Dozen"},
         new Item {Id = 1, Name = "Zebra Z-Grip Retractable Ballpoint", Quantity = 24, Price = 110M, Description = "Zebra Z-Grip Retractable Ballpoint Pen, Medium Point, 1.0mm, Black Ink, 24 Pack"},
         
@@ -41,13 +41,17 @@ namespace InventoryManagementAPI.Services
         }
     }
 
-    public void UpdateItem(Item UpdatedItem)
+    public void UpdateItem(Item updatedItem)
     {
-      existingItem.Name = updatedItem.Name;
-      existingItem.Quantity = updatedItem.Quantity;
-      existingItem.Price = updatedItem.Price;
-      existingItem.Description = updatedItem.Description;
-
+      var existingItem = GetItemById(updatedItem.Id);
+      
+      if (existingItem != null)
+      {
+        existingItem.Name = updatedItem.Name;
+        existingItem.Quantity = updatedItem.Quantity;
+        existingItem.Price = updatedItem.Price;
+        existingItem.Description = updatedItem.Description;
+      }
     }
   }
 
